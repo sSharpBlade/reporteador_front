b1fceb0d66b571e0785a04c9e92cd6fe5b521be8
ramirez7oscar8203 <ramirez7oscar8203@gmail.com>
2024-05-21 07:40:38 +0000

    html y mitad de css para login

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
+import './login.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [singIn] = useState(true);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container" id="container" >
+      <div className='form-container sign-up'>
+        <form>
+          <h1>Crear Cuenta</h1>
+          <div className='social-icons'>
+            <a href="#" className='icon'><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-youtube"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-github"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-linkedin-in"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-facebook"></i></a>
+          </div>
+          <span>o usa tu correo para registrarte</span>
+
+          <input type="text" placeholder="Nombre" />
+          <input type="email" placeholder="Correo" />
+          <input type="password" placeholder="Contraseña" />
+          <button>Registrarse</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='form-container sign-up'>
+        <form>
+          <h1>Iniciar Sesión</h1>
+          <div className='social-icons'>
+            <a href="#" className='icon'><i className="fa-brands fa-google-plus-g"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-youtube"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-github"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-linkedin-in"></i></a>
+            <a href="#" className='icon'><i className="fa-brands fa-facebook"></i></a>
+          </div>
+          <span>o usa tu correo y contraseña</span>
+
+          <input type="email" placeholder="Correo" />
+          <input type="password" placeholder="Contraseña" />
+          <a href="#"> Olvidaste tu contraseña?</a>
+          <button>Iniciar Sesión</button>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="toggle-container">
+        <div className="toggle">
+          <div className="toggle-panel toggle-left">
+            <h1>Bienvenido de nuevo</h1>
+            <p>Ingresa tus datos personales para usar la aplicación</p>
+            <button className="hidden" id='login'>Iniciar Sesión</button>
+          </div>
+          <div className="toggle-panel toggle-right">
+            <h1>Hola, Bienvenido!</h1>
+            <p>Registra tu cuenta para usar la aplicación</p>
+            <button className="hidden" id='register'>Registrarse</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
